f9a427e5c0b79012038ed1426ece0f0f49db4d98
Dandy <dandyjefferson@gmail.com>
2015-01-28 18:32:34 +0000

    Bar.java

---
+++ Bar.java
@@ -0,0 +1,43 @@
+package com.brick;
+
+//本类用于定义球板
+
+public class Bar {
+	private int x;
+	private int y;
+	private int length;
+	
+
+	public Bar() {
+		this.x = 0;
+		this.y = 450;
+		this.length = 200;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+
+}
